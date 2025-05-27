0ce01193c6d49e5f8fafd5e2051c2361b94c530c
Ghazi KERFAHI <ghazi.kerfahi@gmail.com>
2025-05-27 03:00:00 +0200

    change vuejs

--- backend/src/main/java/com/laposte/djl/service/ImageRecognitionService.java
+++ backend/src/main/java/com/laposte/djl/service/ImageRecognitionService.java
@@ -72,16 +72,6 @@ public class ImageRecognitionService {
     @PostConstruct
     public void initModelZoo() {
         try {
-            System.out.println("Start Model Zoo load.");
-            ModelZoo.listModels().forEach(m -> {
-                if (m.getApplication() == Application.CV.IMAGE_CLASSIFICATION
-                        && m.getInputTypes().contains(Image.class)
-                        && m.getOutputTypes().contains(Classifications.class)) {
-                    System.out.println("Model: " + m.getName());
-                    System.out.println("Filters: " + m.getFilters());
-                }
-            });
-
             Criteria<Image, Classifications> criteria = Criteria.builder()
                     .optApplication(Application.CV.IMAGE_CLASSIFICATION)
                     .setTypes(Image.class, Classifications.class)
